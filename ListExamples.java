6b3988e4051334a8876b09f0d594faa87fafb676
HILTON Kim <kim.hilton@soprasteria.com>
2020-09-23 11:18:32 +0200

    See prev commit message

--- ListExamples.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class ListExamples {
-    public static void main(String[] args){
-        ArrayList<Integer> a = new ArrayList(); // valid
-        //ArrayList<> b = new ArrayList<>();  // invalid
-        var c = new ArrayList<>();  // valid
-    }
-}
